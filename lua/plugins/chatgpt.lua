874ce65e50a5cb15823ecb04a0b21efd2e2186af
Andrew Ribeiro <andtankian@gmail.com>
2024-01-11 20:25:54 -0300

    fix lsp

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -1,31 +1,32 @@
 return {
-  "jackMort/ChatGPT.nvim",
-  event = "VeryLazy",
-  opts = {
-    popup_layout = {
-      center = {
-        width = "90%",
-        height = "90%",
-      },
-    },
-    openai_params = {
-      max_tokens = 1000,
-    },
-    openai_edit_params = {
-      max_tokens = 1000,
-    },
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
+	"jackMort/ChatGPT.nvim",
+	event = "VeryLazy",
+	opts = {
+		popup_layout = {
+			center = {
+				width = "90%",
+				height = "90%",
+			},
+		},
+		openai_params = {
+			max_tokens = 4000,
+			model = "gpt-3.5-turbo-1106",
+		},
+		openai_edit_params = {
+			max_tokens = 4000,
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
 
-  config = function(_, opts)
-    require("chatgpt").setup(opts)
+	config = function(_, opts)
+		require("chatgpt").setup(opts)
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>gpt", ":ChatGPT<CR>", { noremap = true, silent = true })
-  end,
+		keymap.set("n", "<leader>gpt", ":ChatGPT<CR>", { noremap = true, silent = true })
+	end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,50 +26,6 @@ return {
 			root_dir = lspconfig.util.find_git_ancestor,
 		})
 
-		-- luasnip setup
-		local luasnip = require("luasnip")
-
-		-- nvim-cmp setup
-		local cmp = require("cmp")
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-u>"] = cmp.mapping.scroll_docs(-4),
-				["<C-d>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-			},
-		})
-
 		-- Global mappings.
 		vim.keymap.set("n", "<leader>f", vim.diagnostic.open_float)
 		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
