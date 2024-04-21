3d79b83ea563f93ada9d54d4aa878839cd05bf2e
Andrew Ribeiro <andtankian@gmail.com>
2024-04-21 18:31:28 -0300

    refactor: change file explorer

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,12 @@
-local keymap = vim.keymap
+local utils = require("utils")
+
 local g = vim.g
 
-keymap.set("n", "<Space>", "")
-keymap.set("n", "<Esc>", "<cmd> noh <CR>")
-keymap.set("n", "<C-h>", "<C-w>h")
-keymap.set("n", "<C-l>", "<C-w>l")
-keymap.set("n", "<C-j>", "<C-w>j")
-keymap.set("n", "<C-k>", "<C-w>k")
+utils.map("n", "<Space>", "")
+utils.map("n", "<Esc>", "<cmd> noh <CR>")
+utils.map("n", "<C-h>", "<C-w>h")
+utils.map("n", "<C-l>", "<C-w>l")
+utils.map("n", "<C-j>", "<C-w>j")
+utils.map("n", "<C-k>", "<C-w>k")
 
 g.mapleader = " "
