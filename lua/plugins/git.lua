80c5a6c2caa97d2d19c9596757468795c922318d
Andrew Ribeiro <andtankian@gmail.com>
2024-04-21 17:28:18 -0300

    refactor: adjust gitsign

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,21 +1,16 @@
 return {
 	{
-		"tpope/vim-fugitive",
+		"tpope/vim-fugitive", -- git functionality
 	},
 	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
+		"lewis6991/gitsigns.nvim", -- git sign
+		event = "BufEnter",
+		keys = {
+			{
+				"]c",
+				function()
+					local gs = require("gitsigns")
 
-				local function map(mode, l, r, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					vim.keymap.set(mode, l, r, opts)
-				end
-
-				-- Navigation
-				map("n", "]c", function()
 					if vim.wo.diff then
 						return "]c"
 					end
@@ -23,9 +18,15 @@ return {
 						gs.next_hunk()
 					end)
 					return "<Ignore>"
-				end, { expr = true })
+				end,
+				desc = "Go to next hunk",
+				{ expr = true },
+			},
+			{
+				"[c",
+				function()
+					local gs = require("gitsigns")
 
-				map("n", "[c", function()
 					if vim.wo.diff then
 						return "[c"
 					end
@@ -33,28 +34,55 @@ return {
 						gs.prev_hunk()
 					end)
 					return "<Ignore>"
-				end, { expr = true })
-
-				-- Actions
-				map("n", "<leader>hs", gs.stage_hunk)
-				map("n", "<leader>hr", gs.reset_hunk)
-				map("v", "<leader>hs", function()
-					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
-				map("v", "<leader>hr", function()
-					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
-				map("n", "<leader>hp", gs.preview_hunk)
-				map("n", "<leader>hb", function()
-					gs.blame_line({ full = true })
-				end)
-				map("n", "<leader>tb", gs.toggle_current_line_blame)
-				map("n", "<leader>hd", gs.diffthis)
-				map("n", "<leader>hD", function()
-					gs.diffthis("~")
-				end)
-				map("n", "<leader>td", gs.toggle_deleted)
-			end,
+				end,
+				desc = "Go to previous hunk",
+				{ expr = true },
+			},
+			{
+				"<leader>hs",
+				function()
+					require("gitsigns").stage_hunk()
+				end,
+				desc = "Stage hunk",
+			},
+			{
+				"<leader>hr",
+				function()
+					require("gitsigns").reset_hunk()
+				end,
+				desc = "Reset hunk",
+			},
+			{
+				"<leader>hp",
+				function()
+					require("gitsigns").preview_hunk()
+				end,
+				desc = "Preview hunk",
+			},
+			{
+				"<leader>hb",
+				function()
+					require("gitsigns").blame_line({ full = true })
+				end,
+				desc = "Blame line",
+			},
+			{
+				"<leader>hd",
+				function()
+					require("gitsigns").diffthis()
+				end,
+				desc = "Diff this",
+			},
+			{
+				"<leader>hD",
+				function()
+					require("gitsigns").diffthis("~")
+				end,
+				desc = "Diff this (against HEAD)",
+			},
+		},
+		opts = {
+			current_line_blame = true,
 		},
 		config = function(_, opts)
 			require("gitsigns").setup(opts)
