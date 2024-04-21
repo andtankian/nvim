9b2f0a5dbd991c56688a016a5b061412936f5cad
Andrew Ribeiro <andtankian@gmail.com>
2024-04-21 17:39:58 -0300

    refactor: bufferline and lualine plugins

--- lua/plugins/lines.lua
+++ lua/plugins/lines.lua
@@ -1,39 +1,48 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local bl = require("bufferline")
-      bl.setup()
-
-      local keymap = vim.keymap
-      local mapping_options = { silent = true, noremap = true }
-
-      function close_all_buffers()
-        for _, e in ipairs(bl.get_elements().elements) do
-          vim.schedule(function()
-            vim.cmd("bd " .. e.id)
-          end)
-        end
-      end
-
-      keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", mapping_options)
-      keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", mapping_options)
-      keymap.set("n", "<leader>x", ":bd|bp <CR>", mapping_options)
-      keymap.set("n", "<leader>X", "<cmd>lua close_all_buffers()<CR>", mapping_options)
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      options = {
-        theme = "dracula",
-      },
-    },
-    config = function(_, opts)
-      require("lualine").setup(opts)
-    end,
-  },
+	{
+		"akinsho/bufferline.nvim", -- tabs to list buffers
+		version = "*",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "BufEnter", -- only load when a buffer is opened
+		keys = {
+			{
+				"<Tab>",
+				":BufferLineCycleNext<CR>",
+				desc = "Go to next buffer",
+			},
+			{
+				"<S-Tab>",
+				":BufferLineCyclePrev<CR>",
+				desc = "Go to previous buffer",
+			},
+			{
+				"<leader>x",
+				":bd|bp <CR>",
+				desc = "Close buffer",
+			},
+			{
+				"<leader>X",
+				function()
+					local bl = require("bufferline")
+					for _, e in ipairs(bl.get_elements().elements) do
+						vim.schedule(function()
+							vim.cmd("bd " .. e.id)
+						end)
+					end
+				end,
+				desc = "Close all buffers",
+			},
+		},
+		opts = {},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		event = "VimEnter",
+		opts = {
+			options = {
+				theme = "dracula",
+			},
+		},
+	},
 }
