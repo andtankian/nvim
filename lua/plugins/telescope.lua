2adcd2820563ec70995c3623a507b1b628e18c65
Andrew Ribeiro <andtankian@gmail.com>
2024-04-22 09:21:26 -0300

    refactor: telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local utils = require("utils")
+
 return {
 	"nvim-telescope/telescope.nvim",
 	-- tag = "0.1.6",
@@ -12,67 +14,71 @@ return {
 			version = "^1.0.0",
 		},
 	},
-	config = function()
+	opts = {
+		defaults = {
+			sorting_strategy = "ascending",
+			selection_strategy = "follow",
+			layout_config = {
+				height = 0.9,
+				width = 0.9,
+				preview_cutoff = 120,
+				prompt_position = "top",
+			},
+			prompt_prefix = " 🔎  ",
+			selection_caret = "  ",
+			multi_icon = "󰐾 ",
+			border = {},
+			path_display = {
+				shorten = {
+					len = 3,
+				},
+			},
+			mappings = {
+				i = {
+					["<C-Down>"] = require("telescope.actions").cycle_history_next,
+					["<C-Up>"] = require("telescope.actions").cycle_history_prev,
+				},
+			},
+		},
+		extensions = {
+			fzf = {
+				fuzzy = false,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
+			},
+		},
+	},
+	keys = {
+		{ "<leader>ff", require("telescope.builtin").find_files, desc = "Find files" },
+		{ "<leader>fb", require("telescope.builtin").buffers, desc = "List buffers" },
+		{
+			"<leader>fw",
+			function()
+				local telescope = require("telescope")
+				utils.map("n", "<leader>fw", telescope.extensions.live_grep_args.live_grep_args, {})
+			end,
+		},
+		{
+			"<leader>gc",
+			function()
+				local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+				utils.map("n", "<leader>gc", live_grep_args_shortcuts.grep_word_under_cursor, {})
+			end,
+			desc = "Grep word under cursor",
+		},
+		{ "<leader>gt", require("telescope.builtin").git_status, desc = "Git status" },
+		{ "<leader>fgb", require("telescope.builtin").git_branches, desc = "Git branches" },
+		{ "<leader>fgs", require("telescope.builtin").git_stash, desc = "Git stash" },
+		{ "<leader>fgc", require("telescope.builtin").git_commits, desc = "Git commits" },
+		{ "<leader>fgh", require("telescope.builtin").git_bcommits, desc = "Git bcommits" },
+		{ "<leader>fr", require("telescope.builtin").resume, desc = "Resume last telescope" },
+		{ "<leader>fo", require("telescope.builtin").oldfiles, desc = "Old files" },
+	},
+	config = function(_, opts)
 		local telescope = require("telescope")
-
-		telescope.setup({
-			defaults = {
-				sorting_strategy = "ascending",
-				selection_strategy = "follow",
-				layout_config = {
-					height = 0.9,
-					width = 0.9,
-					preview_cutoff = 120,
-					prompt_position = "top",
-				},
-				prompt_prefix = " 🔎  ",
-				selection_caret = "  ",
-				multi_icon = "󰐾 ",
-				border = {},
-				path_display = {
-					shorten = {
-						len = 3,
-					},
-				},
-				mappings = {
-					i = {
-						["<C-Down>"] = require("telescope.actions").cycle_history_next,
-						["<C-Up>"] = require("telescope.actions").cycle_history_prev,
-					},
-				},
-			},
-			pickers = {
-				git_status = {
-					selection_strategy = "follow",
-				},
-			},
-			extensions = {
-				fzf = {
-					fuzzy = false,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
-			},
-		})
-
+		telescope.setup(opts)
 		telescope.load_extension("fzf")
 		telescope.load_extension("live_grep_args")
-
-		local set = vim.keymap.set
-		local builtin = require("telescope.builtin")
-		local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
-
-		set("n", "<leader>ff", builtin.find_files, {})
-		set("n", "<leader>fb", builtin.buffers, {})
-		set("n", "<leader>fw", telescope.extensions.live_grep_args.live_grep_args, {})
-		set("n", "<leader>gt", builtin.git_status, {})
-		set("n", "<leader>fgb", builtin.git_branches, {})
-		set("n", "<leader>fgs", builtin.git_stash, {})
-		set("n", "<leader>fgc", builtin.git_commits, {})
-		set("n", "<leader>fgh", builtin.git_bcommits, {})
-		set("n", "<leader>gc", live_grep_args_shortcuts.grep_word_under_cursor, {})
-		set("n", "<leader>fr", builtin.resume, {})
-		set("n", "<leader>fo", builtin.oldfiles, {})
 	end,
 }
