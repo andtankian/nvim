5ce3d09613de7a8aa72e1023babca3d64650c6fd
Andrew Ribeiro <andtankian@gmail.com>
2024-05-08 22:34:41 -0300

    chore: add todos and add new ripgrep arguments

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -57,6 +57,20 @@ return {
 				override_file_sorter = true,
 				case_mode = "smart_case",
 			},
+			live_grep_args = {
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+					"--glob",
+					"!**/.git/*",
+				},
+			},
 		},
 	},
 	keys = {
